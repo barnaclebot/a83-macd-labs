1651e24a2d6ac2cbf438dbad4eb6ed4e7e7b2eb2
barnaclebot <al.asepnug@gmail.com>
2019-04-22 23:40:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "barnaclebotwebappserver.database.windows.net";
+    $user = "barnaclebot";
+    $pass = "Ahbabulmustofa3";
+    $db = "webdb";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
